c27668e64f0d352c47402f1a081925ef99388a77
DarMatt <daryamatviienko@gmail.com>
2021-01-12 23:42:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.30aa7e2f2798d16fecb6a87e6173572d.js"
 );
 
 self.addEventListener('message', (event) => {
